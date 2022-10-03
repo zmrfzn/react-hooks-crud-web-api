fa3cfb4f55adbc58de9bf0bdabd24cd43a9752be
Zameer Fouzan <zmrfzn@gmail.com>
2022-10-03 20:14:21 +0530

    API config for deployment

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const APIUrl = `${process.env.REACT_APP_API_URL}`
+
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: APIUrl,
   headers: {
     "Content-type": "application/json"
   }
