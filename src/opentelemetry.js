f3a3d5ee28a7b3e54d011a70cce0c514847c704e
Zameer Fouzan <zmrfzn@gmail.com>
2023-01-25 13:29:44 +0530

    working Otel version - CORS fix - configuration consolidation

--- src/opentelemetry.js
+++ src/opentelemetry.js
@@ -26,60 +26,51 @@ import {
 } from "@opentelemetry/sdk-trace-base";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 
+const collectorString = "https://otlp.nr-data.net:4318/v1/traces";
+const provider = new WebTracerProvider({
+resource: new Resource({
+  [SemanticResourceAttributes.SERVICE_NAME]: "react-tutorials-otel",
+}),
+});
+
+//consoleExporter for debugging
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+provider.addSpanProcessor(
+new BatchSpanProcessor(
+  new OTLPTraceExporter({
+    url: collectorString || "http://localhost:4318/v1/traces",
+    headers: {
+      "api-key": "f5644626eef13f26d27746c6e381555ef9f9NRAL",
+    },
+  }),
+   //BatchSpanProcessor Configurations
+    {
+      // The maximum queue size. After the size is reached spans are dropped.
+      maxQueueSize: 100,
+      // The maximum batch size of every export. It must be smaller or equal to maxQueueSize.
+      maxExportBatchSize: 50,
+      // The interval between two consecutive exports
+      scheduledDelayMillis: 500,
+      // How long the export can run before it is canceled
+      exportTimeoutMillis: 30000,
+    }
+)
+);
+
+provider.register({
+contextManager: new ZoneContextManager(),
+propagator: new CompositePropagator({
+  propagators: [new W3CBaggagePropagator(), new W3CTraceContextPropagator()],
+}),
+});
+
+// const baggage =   otelApi.propagation.getBaggage(otelApi.context.active()) || otelApi.propagation.createBaggage();
+// debugger;
+// baggage.setEntry("sessionId", { value: "session-id-value" });
+// otelApi.propagation.setBaggage(otelApi.context.active(), baggage);
+
 const startOtelInstrumentation = () => {
-  const providerConfig = {
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: "react-tutorials-otel",
-    }),
-  };
-  const provider = new WebTracerProvider(providerConfig);
-
-  provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-  // provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter()));
-
-  // provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter({
-  //     url:'http://localhost:4318/v1/traces'
-  // })));
-  provider.addSpanProcessor(
-    new BatchSpanProcessor(
-      // OTLP Configurations
-      new OTLPTraceExporter({
-        url: "https://otlp.nr-data.net:4318/v1/traces",
-        headers: {
-          "api-key": `${process.env.NR_LICENSE}`,
-        },
-      }),
-      // BatchSpanProcessor Configurations
-      {
-        // The maximum queue size. After the size is reached spans are dropped.
-        maxQueueSize: 100,
-        // The maximum batch size of every export. It must be smaller or equal to maxQueueSize.
-        maxExportBatchSize: 50,
-        // The interval between two consecutive exports
-        scheduledDelayMillis: 500,
-        // How long the export can run before it is canceled
-        exportTimeoutMillis: 30000,
-      }
-    )
-  );
-
-  provider.register({
-    // Changing default contextManager to use ZoneContextManager - supports asynchronous operations - optional
-    contextManager: new ZoneContextManager().enable(),
-    propagator: new CompositePropagator({
-      propagators: [
-        new W3CBaggagePropagator(),
-        new W3CTraceContextPropagator(),
-        new B3Propagator(),
-      ],
-    }),
-  });
-
-  // const baggage =   otelApi.propagation.getBaggage(otelApi.context.active()) || otelApi.propagation.createBaggage();
-  // debugger;
-  // baggage.setEntry("sessionId", { value: "session-id-value" });
-  // otelApi.propagation.setBaggage(otelApi.context.active(), baggage);
-
   console.error(`Registering Otel ${new Date().getMilliseconds()}`);
   // Registering instrumentations
   registerInstrumentations({
